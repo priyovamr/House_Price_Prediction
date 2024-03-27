f33ee31ac1ee64e39ad1647db698fad9fc4048c2
Priyova Muhammad Rafief <83563157+priyovamr@users.noreply.github.com>
2024-03-28 02:53:41 +0700

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,21 +9,21 @@ st.title("Prediksi Harga Rumah di Kota Semarang")
 col1, col2 = st.columns(2)
 
 with col1:
-    Jenis_Rumah = st.text_input('Input Jenis Rumah')
+    Jenis_Rumah = st.number_input('Input Jenis Rumah')
 with col2:
-    Lokasi = st.text_input('Input Lokasi')
+    Lokasi = st.number_input('Input Lokasi')
 with col1:
-    KT = st.text_input('Input Jumlah Kamar Tidur')
+    KT = st.number_input('Input Jumlah Kamar Tidur')
 with col2:
-    KM = st.text_input('Input Jumlah Kamar Mandi')
+    KM = st.number_input('Input Jumlah Kamar Mandi')
 with col1:
-    Garasi = st.text_input('Input Garasi')
+    Garasi = st.number_input('Input Garasi')
 with col2:
-    LT = st.text_input('Input Luas Tanah')
+    LT = st.number_input('Input Luas Tanah')
 with col1:
-    LB = st.text_input('Input Luas Bangunan')
+    LB = st.number_input('Input Luas Bangunan')
 with col2:
-    KPR_Bulanan = st.text_input('Input harga KPR bulanan')
+    KPR_Bulanan = st.number_input('Input harga KPR bulanan')
 
 predict = ''
 
